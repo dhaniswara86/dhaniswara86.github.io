131c27411f17d0cbb0068bc575dfbb046f2bab15
dhaniswara86 <angga.dhaniswara@gmail.com>
2026-09-09 22:43:38 +0700

    Add files via upload

--- sertifikat-edukasi/qr/qr-generator.js
+++ sertifikat-edukasi/qr/qr-generator.js
@@ -1,39 +1,45 @@
 const logo="assets/logo-djp.png";
 
-function qr(id,url){
- const el=document.getElementById(id);
- el.innerHTML="";
- QRCode.toCanvas(url,{width:320,errorCorrectionLevel:"H"},function(err,canvas){
-   if(err){console.error(err);return;}
-   el.appendChild(canvas);
-   const ctx=canvas.getContext("2d");
-   const img=new Image();
-   img.src=logo;
-   img.onload=function(){
-     let s=55;
-     let x=(canvas.width-s)/2;
-     let y=(canvas.height-s)/2;
-     ctx.fillStyle="#fff";
-     ctx.fillRect(x-8,y-8,s+16,s+16);
-     ctx.drawImage(img,x,y,s,s);
-   };
+function makeQR(id,url){
+ const box=document.getElementById(id);
+ box.innerHTML="";
+ QRCode.toCanvas(url,{width:320,errorCorrectionLevel:"H"},(err,canvas)=>{
+  if(err){console.error(err);return;}
+  box.appendChild(canvas);
+  const ctx=canvas.getContext("2d");
+  const img=new Image();
+  img.src=logo;
+  img.onload=()=>{
+   let s=55,x=(canvas.width-s)/2,y=(canvas.height-s)/2;
+   ctx.fillStyle="#fff";
+   ctx.fillRect(x-8,y-8,s+16,s+16);
+   ctx.drawImage(img,x,y,s,s);
+  };
  });
 }
 
 function generateAll(){
- let c=document.getElementById("eventCode").value;
- qr("qr1","../awal.html?kode="+c);
- qr("qr2","../akhir.html?kode="+c);
- qr("qr3","../verifikasi.html?kode="+c);
+ const c=document.getElementById("eventCode").value;
+ const l1=location.origin+"/sertifikat-edukasi/awal.html?kode="+c;
+ const l2=location.origin+"/sertifikat-edukasi/akhir.html?kode="+c;
+ document.getElementById("link1").value=l1;
+ document.getElementById("link2").value=l2;
+ makeQR("qr1",l1);
+ makeQR("qr2",l2);
 }
 
 function downloadQR(id,name){
- let canvas=document.querySelector("#"+id+" canvas");
- if(!canvas){alert("Generate QR dahulu");return;}
- let a=document.createElement("a");
+ const canvas=document.querySelector("#"+id+" canvas");
+ if(!canvas)return alert("Generate QR dahulu");
+ const a=document.createElement("a");
  a.download=name+".png";
- a.href=canvas.toDataURL("image/png");
+ a.href=canvas.toDataURL();
  a.click();
+}
+
+function copyLink(id){
+ navigator.clipboard.writeText(document.getElementById(id).value);
+ alert("Link berhasil disalin");
 }
 
 window.onload=generateAll;
