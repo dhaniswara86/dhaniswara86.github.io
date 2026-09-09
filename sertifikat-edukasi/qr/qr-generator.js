65524b2e58d8377ccd81514c0d9b24b69e071d16
dhaniswara86 <angga.dhaniswara@gmail.com>
2026-09-09 21:01:04 +0700

    Add files via upload

--- sertifikat-edukasi/qr/qr-generator.js
+++ sertifikat-edukasi/qr/qr-generator.js
@@ -1,65 +1,39 @@
-const QR_LOGO = "../assets/logo-djp.png";
+const logo="assets/logo-djp.png";
 
-function createQR(target, url){
-
-    const box = document.getElementById(target);
-    box.innerHTML = "";
-
-    const qrBox = document.createElement("div");
-    box.appendChild(qrBox);
-
-    new QRCode(qrBox, {
-        text: url,
-        width: 280,
-        height: 280,
-        correctLevel: QRCode.CorrectLevel.H
-    });
-
+function qr(id,url){
+ const el=document.getElementById(id);
+ el.innerHTML="";
+ QRCode.toCanvas(url,{width:320,errorCorrectionLevel:"H"},function(err,canvas){
+   if(err){console.error(err);return;}
+   el.appendChild(canvas);
+   const ctx=canvas.getContext("2d");
+   const img=new Image();
+   img.src=logo;
+   img.onload=function(){
+     let s=55;
+     let x=(canvas.width-s)/2;
+     let y=(canvas.height-s)/2;
+     ctx.fillStyle="#fff";
+     ctx.fillRect(x-8,y-8,s+16,s+16);
+     ctx.drawImage(img,x,y,s,s);
+   };
+ });
 }
-
 
 function generateAll(){
-
-    const code = document.getElementById("eventCode").value;
-
-    createQR(
-        "qr1",
-        "https://dhaniswara86.id/sertifikat-edukasi/awal.html?kode="
-        + encodeURIComponent(code)
-    );
-
-
-    createQR(
-        "qr2",
-        "https://dhaniswara86.id/sertifikat-edukasi/akhir.html?kode="
-        + encodeURIComponent(code)
-    );
-
-
-    createQR(
-        "qr3",
-        "https://dhaniswara86.id/sertifikat-edukasi/verifikasi.html?kode="
-        + encodeURIComponent(code)
-    );
+ let c=document.getElementById("eventCode").value;
+ qr("qr1","../awal.html?kode="+c);
+ qr("qr2","../akhir.html?kode="+c);
+ qr("qr3","../verifikasi.html?kode="+c);
 }
 
-
-function downloadQR(id){
-
-    const img = document.querySelector("#"+id+" img");
-
-    if(!img){
-        alert("QR belum dibuat");
-        return;
-    }
-
-    const a=document.createElement("a");
-    a.download=id+"-kabayan.png";
-    a.href=img.src;
-    a.click();
+function downloadQR(id,name){
+ let canvas=document.querySelector("#"+id+" canvas");
+ if(!canvas){alert("Generate QR dahulu");return;}
+ let a=document.createElement("a");
+ a.download=name+".png";
+ a.href=canvas.toDataURL("image/png");
+ a.click();
 }
 
-
-window.onload=function(){
-    generateAll();
-}
+window.onload=generateAll;
