86cf79ea4de4b39ea9c28dab34e93ce85995ab48
dhaniswara86 <angga.dhaniswara@gmail.com>
2026-09-10 20:33:17 +0700

    Add files via upload

--- sertifikat-edukasi/admin/qr-menu.js
+++ sertifikat-edukasi/admin/qr-menu.js
@@ -1,134 +1,75 @@
-const SUPABASE_URL = "https://ndqwmxshryqpygmupcnj.supabase.co";
-const SUPABASE_KEY = "sb_publishable_-BGFKcxGME4yXqX4vRtWpA_g0-Cldkg";
+const SUPABASE_URL="https://ndqwmxshryqpygmupcnj.supabase.co";
+const SUPABASE_KEY="sb_publishable_-BGFKcxGME4yXqX4vRtWpA_g0-Cldkg";
 
-const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
+const supabaseClient=supabase.createClient(SUPABASE_URL,SUPABASE_KEY);
 
+let selected=null;
 
 async function loadEvents(){
+ const {data,error}=await supabaseClient.from("external_events")
+ .select("id,code,title,event_date")
+ .order("event_date",{ascending:false});
 
-    const {data,error}=await supabaseClient
-        .from("external_events")
-        .select("id,code,title,event_date")
-        .order("event_date",{ascending:false});
+ const s=document.getElementById("eventSelect");
+ s.innerHTML="";
 
+ if(error){s.innerHTML="<option>Gagal mengambil data</option>";return;}
 
-    const select=document.getElementById("eventSelect");
-
-    select.innerHTML="";
-
-
-    if(error){
-        console.error(error);
-        select.innerHTML="<option>Gagal mengambil kegiatan</option>";
-        return;
-    }
-
-
-    data.forEach(item=>{
-
-        const option=document.createElement("option");
-
-        option.value=item.id;
-        option.dataset.code=item.code;
-        option.dataset.title=item.title;
-        option.dataset.date=item.event_date;
-
-        option.textContent =
-            item.code+" - "+item.title+
-            " ("+item.event_date+")";
-
-        select.appendChild(option);
-
-    });
-
+ data.forEach(e=>{
+  let o=document.createElement("option");
+  o.value=e.id;
+  o.dataset.code=e.code;
+  o.dataset.title=e.title;
+  o.textContent=e.code+" - "+e.title;
+  s.appendChild(o);
+ });
 }
-
 
 function generateQR(){
+ const s=document.getElementById("eventSelect");
+ const e=s.options[s.selectedIndex];
 
-    const select=document.getElementById("eventSelect");
-    const option=select.options[select.selectedIndex];
+ if(!e)return;
 
-    if(!option){
-        alert("Pilih kegiatan terlebih dahulu");
-        return;
-    }
+ selected=e;
 
+ const l1=location.origin+"/sertifikat-edukasi/awal.html?id="+e.value;
+ const l2=location.origin+"/sertifikat-edukasi/akhir.html?id="+e.value;
 
-    const id=option.value;
+ document.getElementById("link1").value=l1;
+ document.getElementById("link2").value=l2;
 
-    document.getElementById("eventDetail").innerHTML =
-    "<b>"+option.dataset.code+"</b><br>"+
-    option.dataset.title+"<br>"+
-    option.dataset.date;
+ makeQR("qr1",l1);
+ makeQR("qr2",l2);
 
-
-    const link1 =
-    location.origin+
-    "/sertifikat-edukasi/awal.html?id="+
-    encodeURIComponent(id);
-
-
-    const link2 =
-    location.origin+
-    "/sertifikat-edukasi/akhir.html?id="+
-    encodeURIComponent(id);
-
-
-    document.getElementById("link1").value=link1;
-    document.getElementById("link2").value=link2;
-
-
-    makeQR("qr1",link1);
-    makeQR("qr2",link2);
-
+ addHistory(e.dataset.code,"Daftar Hadir + Pretest");
+ addHistory(e.dataset.code,"Posttest + Evaluasi");
 }
 
-
-function makeQR(target,text){
-
-    const box=document.getElementById(target);
-    box.innerHTML="";
-
-
-    new QRCode(box,{
-        text:text,
-        width:260,
-        height:260,
-        correctLevel:QRCode.CorrectLevel.H
-    });
-
+function makeQR(id,text){
+ const box=document.getElementById(id);
+ box.innerHTML="";
+ new QRCode(box,{text:text,width:260,height:260,correctLevel:QRCode.CorrectLevel.H});
 }
 
+function addHistory(code,type){
+ let row=document.createElement("tr");
+ row.innerHTML="<td>"+new Date().toLocaleString()+"</td><td>"+code+"</td><td>"+type+"</td>";
+ document.getElementById("history").prepend(row);
+}
 
 function copyLink(id){
-
-    navigator.clipboard.writeText(
-        document.getElementById(id).value
-    );
-
-    alert("Link berhasil disalin");
+ navigator.clipboard.writeText(document.getElementById(id).value);
+ alert("Link disalin");
 }
-
 
 function downloadQR(id,name){
-
-    const img=document.querySelector("#"+id+" img");
-
-    if(!img){
-        alert("Generate QR terlebih dahulu");
-        return;
-    }
-
-    const a=document.createElement("a");
-    a.href=img.src;
-    a.download=name+".png";
-    a.click();
-
+ const img=document.querySelector("#"+id+" img");
+ if(!img)return;
+ let a=document.createElement("a");
+ a.href=img.src;
+ a.download=name+".png";
+ a.click();
 }
-
 
 window.onload=loadEvents;
