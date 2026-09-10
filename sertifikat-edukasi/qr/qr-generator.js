46302a099d63e51a0c34847c339098725d7e0b65
dhaniswara86 <angga.dhaniswara@gmail.com>
2026-09-10 07:43:22 +0700

    Update qr-generator.js

--- sertifikat-edukasi/qr/qr-generator.js
+++ sertifikat-edukasi/qr/qr-generator.js
@@ -1,45 +1,64 @@
 const logo="assets/logo-djp.png";
 
 function makeQR(id,url){
+
  const box=document.getElementById(id);
  box.innerHTML="";
- QRCode.toCanvas(url,{width:320,errorCorrectionLevel:"H"},(err,canvas)=>{
-  if(err){console.error(err);return;}
-  box.appendChild(canvas);
-  const ctx=canvas.getContext("2d");
-  const img=new Image();
-  img.src=logo;
-  img.onload=()=>{
-   let s=55,x=(canvas.width-s)/2,y=(canvas.height-s)/2;
-   ctx.fillStyle="#fff";
-   ctx.fillRect(x-8,y-8,s+16,s+16);
-   ctx.drawImage(img,x,y,s,s);
-  };
- });
-}
 
-function generateAll(){
- const c=document.getElementById("eventCode").value;
- const l1=location.origin+"/sertifikat-edukasi/awal.html?kode="+c;
- const l2=location.origin+"/sertifikat-edukasi/akhir.html?kode="+c;
- document.getElementById("link1").value=l1;
- document.getElementById("link2").value=l2;
- makeQR("qr1",l1);
- makeQR("qr2",l2);
-}
+ if(typeof QRCode === "undefined"){
+   alert("Library QR Code belum terbaca");
+   return;
+ }
 
-function downloadQR(id,name){
- const canvas=document.querySelector("#"+id+" canvas");
- if(!canvas)return alert("Generate QR dahulu");
- const a=document.createElement("a");
- a.download=name+".png";
- a.href=canvas.toDataURL();
- a.click();
-}
+ QRCode.toCanvas(
+   url,
+   {
+     width:320,
+     margin:2,
+     errorCorrectionLevel:"H"
+   },
+   function(err,canvas){
 
-function copyLink(id){
- navigator.clipboard.writeText(document.getElementById(id).value);
- alert("Link berhasil disalin");
-}
+     if(err){
+       console.error(err);
+       return;
+     }
 
-window.onload=generateAll;
+     box.appendChild(canvas);
+
+     const ctx=canvas.getContext("2d");
+     const img=new Image();
+
+     img.src="../assets/logo-djp.png";
+
+     img.onload=function(){
+
+       const size=55;
+       const x=(canvas.width-size)/2;
+       const y=(canvas.height-size)/2;
+
+       ctx.fillStyle="#ffffff";
+       ctx.beginPath();
+       ctx.arc(
+          canvas.width/2,
+          canvas.height/2,
+          35,
+          0,
+          Math.PI*2
+       );
+       ctx.fill();
+
+       ctx.drawImage(
+          img,
+          x,
+          y,
+          size,
+          size
+       );
+
+     };
+
+   }
+ );
+
+}
